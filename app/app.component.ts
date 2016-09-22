253b951d75ae696c24a2efc800c7a41203924f49
Priyanka <singhpriyanka1@gmail.com>
2016-09-22 09:50:29 -0700

    pushing gitignore

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>My third Angular 2 App</h1>'
+    template: '<h1>My fourth Angular 2 App</h1>'
 })
 export class AppComponent { }
